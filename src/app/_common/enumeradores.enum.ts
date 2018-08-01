0cd43f1d613950f3f3f124824a37e2aaa6b93019
Ter4_LugelFranco <lugel1996@gmail.com>
2018-08-01 04:53:05 -0500

    Se agrega enumerador para el tipo de viaje y para el nombre de los meses

--- src/app/_common/enumeradores.enum.ts
+++ src/app/_common/enumeradores.enum.ts
@@ -39,3 +39,24 @@ export const Transport = {
     1 : 'Aéreo',
     2 : 'Terrestre'
 }
+
+export const Months = {
+    1 : 'Enero',
+    2 : 'Febrero',
+    3 : 'Marzo',
+    4 : 'Abril',
+    5 : 'Mayo',
+    6 : 'Junio',
+    7 : 'Julio',
+    8 : 'Agosto',
+    9 : 'Septiembre',
+    10 : 'Octubre',
+    11 : 'Noviembre',
+    12 : 'Diciembre'
+}
+
+export enum TravelType{
+    GruopOut = 1,
+    GroupFlyOut = 2,
+    AloneOut = 3
+}
