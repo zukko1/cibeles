a7e8efdb30e6c37515ca8a6e3d03370f53048e9d
Ter4_LugelFranco <lugel1996@gmail.com>
2018-08-01 06:49:26 -0500

    Se inicia el listado de los viajes en la vista de los planes

--- src/app/componentes/planes/planes.component.ts
+++ src/app/componentes/planes/planes.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Travel } from '../../_models/_entity-models/travel';
+import { TravelController } from '../../_controllers/travel.controller';
 
 @Component({
   selector: 'app-planes',
   templateUrl: './planes.component.html',
   styleUrls: ['./planes.component.css']
 })
-export class PlanesComponent implements OnInit {
+export class PlanesComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public groupTravels : Travel[];
+  public groupFlyTravels : Travel[];
+  public aloneTravels : Travel[];
+
+
+  constructor(
+    public travelController : TravelController
+  ) { }
+
+  getDays(startDate : Date, finishDate : Date){
+    return 0;
+  }
 
   ngOnInit() {
+    this.travelController.loadTravels();
+
+    this.travelController.groupSubject.subscribe(
+      travels => {
+        this.groupTravels = travels;
+        console.log(this.groupTravels);
+      }
+    );
+
+    this.travelController.aloneSubject.subscribe(
+      travels => {
+        this.aloneTravels = travels;
+        console.log(this.aloneTravels);
+      }
+    )
+
+    this.travelController.groupFlySubject.subscribe(
+      travels => {
+        this.groupFlyTravels = travels;
+        console.log(this.groupFlyTravels);
+      }
+    )
   }
+
+  ngOnDestroy(): void {
+    this.travelController.groupSubject.unsubscribe();
+    this.travelController.starredSubject.unsubscribe();
+  }
+
 
 }
